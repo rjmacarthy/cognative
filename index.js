4c1263bf483341e9fd5cb94b11e2c1cbc5f65b1d
rjmacarthy <richardmacarthy@protonmail.com>
2022-09-30 13:36:41 +0100

    export from index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// todo
+import * as math from "./lib/math"
+import * as base from "./lib/base"
+import * as rotate from "./lib/rotate"
+import * as shape from "./lib/shape"
+
+export default {
+    base,
+    math,
+    rotate,
+    shape
+  }
