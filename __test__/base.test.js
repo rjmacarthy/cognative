a6dd12d2d4631b36d881725dd8cca3152fe06c4e
rjmacarthy <richardmacarthy@protonmail.com>
2022-10-07 10:44:06 +0100

    add missing tests

--- __test__/base.test.js
+++ __test__/base.test.js
@@ -1,4 +1,4 @@
-import { compose, sFn, isSquare, swap } from "../lib/base"
+import { compose, sFn, isSquare, swap, sFill } from "../lib/base"
 
 test("base", () => {
   expect(
@@ -8,6 +8,8 @@ test("base", () => {
       (a) => a + 1
     )(0)
   ).toEqual(3)
+
+  expect(sFill(3, 1)).toEqual([1, 1, 1])
 
   expect(sFn([1, 2, 3], (x) => x * 2)).toEqual([2, 4, 6])
 
--- __test__/determinant.test.js
+++ __test__/determinant.test.js
@@ -1,6 +1,11 @@
 import { determinant } from "../lib/algorithms/determinant"
 
 test("base", () => {
+
+  expect(
+    determinant([[1,2,3], [1,2,3]])
+  ).toEqual(0)
+
   expect(
     determinant([
       [8, 15],
