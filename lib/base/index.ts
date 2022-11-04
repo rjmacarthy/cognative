4958c9344ba3be4b331507fe6a95690b72294692
rjmacarthy <richardmacarthy@protonmail.com>
2022-11-04 15:48:15 +0000

    lint

--- lib/base/index.ts
+++ lib/base/index.ts
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import * as _ from 'lodash'
+import * as _ from "lodash"
 
-export const compose = (...fns: Function[]) =>
-    (args: Matrix) =>
-      _.reduce(fns, (arg, fn: Function) => fn(arg), args)
+export const compose =
+  (...fns: Function[]) =>
+  (args: Matrix) =>
+    _.reduce(fns, (arg, fn: Function) => fn(arg), args)
 
-  
 export const sFn = (x: Matrix) => (fn: (s: Series) => SeriesResult) =>
   _.map(
     x,
@@ -21,13 +21,18 @@ export const swap = (arr: number[], i: number, j: number) => {
   return arr
 }
 
-export const transpose = (x : Matrix) => _.map(_.first(x), (_v: Series, i: number): number[] => _.reverse(_.map(x, (r) => r[i])))
+export const transpose = (x: Matrix) =>
+  _.map(_.first(x), (_v: Series, i: number): number[] =>
+    _.reverse(_.map(x, (r) => r[i]))
+  )
 
-export const inverseTranspose = _.memoize((x : Matrix) =>
-  _.map(_.first(x), (_v: Series, i: number) => _.map(x, (r: number[]) => r[_.size(r) - 1 - i]))
+export const inverseTranspose = _.memoize((x: Matrix) =>
+  _.map(_.first(x), (_v: Series, i: number) =>
+    _.map(x, (r: number[]) => r[_.size(r) - 1 - i])
+  )
 )
 
-export const shape = _.memoize((x : Matrix) => [_.size(x), _.size(_.first(x))])
+export const shape = _.memoize((x: Matrix) => [_.size(x), _.size(_.first(x))])
 
 export const isSquare = _.memoize(
   (x) => _.get(shape(x), 0) === _.get(shape(x), 1)
