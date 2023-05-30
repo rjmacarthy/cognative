877b087281280ef20f8988cba7b8d923c9418a9e
rjmacarthy <richardmacarthy@hotmail.com>
2023-05-30 21:47:01 +0100

    rename

--- __test__/linear.test.ts
+++ __test__/linear.test.ts
@@ -1,7 +1,7 @@
-import { LinearLayer } from '../lib/core/linear'
+import { Linear } from '../lib/core/linear'
 
 describe('linear', () => {
 
-  const ll = new LinearLayer(3, 2)
+  const ll = new Linear(3, 2)
   console.log(ll.forward([1, 2, 3]))
 })
